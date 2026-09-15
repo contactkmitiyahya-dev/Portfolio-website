89bbd9f8d1c5e6b74eb99d6c25303c07ab9376d6
contactkmitiyahya-dev <contactkmitiyahya@gmail.com>
2026-09-15 13:06:49 +0100

    production test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
+//
 export default defineConfig({
   plugins: [
     // react(),
